dea58826ce6e015ab9f2e281bb27c4209c5aac63
samuel-asleep <benneu40@gmail.com>
2026-06-22 16:13:14 +0100

    update

---
+++ src/common/ara-ipc/ARAChannelFormat.cpp
@@ -0,0 +1,241 @@
+//------------------------------------------------------------------------------
+//! \file       ARAChannelFormat.cpp
+//!             utility class dealing with the companion-API-dependent surround channel arrangements
+//! \project    ARA SDK Library
+//! \copyright  Copyright (c) 2021-2026, Celemony Software GmbH, All Rights Reserved.
+//! \license    Licensed under the Apache License, Version 2.0 (the "License");
+//!             you may not use this file except in compliance with the License.
+//!             You may obtain a copy of the License at
+//!
+//!               http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!             Unless required by applicable law or agreed to in writing, software
+//!             distributed under the License is distributed on an "AS IS" BASIS,
+//!             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!             See the License for the specific language governing permissions and
+//!             limitations under the License.
+//------------------------------------------------------------------------------
+
+#include "ARAChannelFormat.h"
+
+#include "ARA_Library/Debug/ARADebug.h"
+
+#include <cstring>
+
+#if defined (__APPLE__)
+    // \todo hotfix: JUCE #defines Point as juce::Point, which clashes with QuickDraw's Point in MacTypes.h
+    #if defined (Point)
+        #undef Point
+    #endif
+
+    #include <CoreAudio/CoreAudioTypes.h>
+#endif
+
+namespace ARA {
+
+void ChannelFormat::update (const ARAChannelCount channelCount,
+                            const ARAChannelArrangementDataType channelArrangementDataType,
+                            const void* const channelArrangement)
+{
+    const auto oldSize { getChannelArrangementDataSize (_channelCount, _channelArrangementDataType, _channelArrangement) };
+    const auto newSize { getChannelArrangementDataSize (channelCount, channelArrangementDataType, channelArrangement) };
+    const bool needsAllocatedStorage { newSize > sizeof (_arrangementStorage) };
+
+    bool hasAllocatedStorage = (_channelArrangement != nullptr) && (_channelArrangement != _arrangementStorage);
+    if (hasAllocatedStorage)
+    {
+        if ((oldSize < newSize) || !needsAllocatedStorage)
+        {
+            delete[] static_cast<ARAByte *> (_channelArrangement);
+            hasAllocatedStorage = false;
+        }
+    }
+
+    _channelCount = channelCount;
+    _channelArrangementDataType = channelArrangementDataType;
+
+    if (newSize > 0)
+    {
+        if (!needsAllocatedStorage)
+            _channelArrangement = &_arrangementStorage;
+        else if (!hasAllocatedStorage)
+            _channelArrangement = new ARAByte[newSize];
+
+        std::memcpy (_channelArrangement, channelArrangement, newSize);
+    }
+    else
+    {
+        _channelArrangement = nullptr;
+    }
+}
+
+bool ChannelFormat::operator== (const ChannelFormat& other) const noexcept
+{
+    if (_channelCount != other._channelCount)
+        return false;
+
+    if (_channelArrangementDataType != other._channelArrangementDataType)
+        return false;
+
+    const auto size { getChannelArrangementDataSize (_channelCount, _channelArrangementDataType, _channelArrangement) };
+    if (size != getChannelArrangementDataSize (other._channelCount, other._channelArrangementDataType, other._channelArrangement))
+        return false;
+
+    if (size == 0)
+        return true;
+
+    return (0 == std::memcmp (_channelArrangement, other._channelArrangement, size));
+}
+
+ARASize ChannelFormat::getChannelArrangementDataSize (const ARAChannelCount channelCount,
+                                                      const ARAChannelArrangementDataType channelArrangementDataType,
+                                                      const void* const channelArrangement) noexcept
+{
+    if (channelArrangement == nullptr)
+        return 0;
+
+    switch (channelArrangementDataType)
+    {
+        case kARAChannelArrangementUndefined:
+        {
+            return 0;
+        }
+        case kARAChannelArrangementVST3SpeakerArrangement:
+        {
+            return sizeof (uint64_t);
+        }
+        case kARAChannelArrangementCoreAudioChannelLayout:
+        {
+#if defined (__APPLE__)
+            static_assert (offsetof (AudioChannelLayout, mChannelDescriptions) <= sizeof (_arrangementStorage),
+                           "storage should be large enough to store all non-channel-description based layouts");
+            const auto audioChannelLayout { static_cast<const AudioChannelLayout*> (channelArrangement) };
+            return offsetof (AudioChannelLayout, mChannelDescriptions) +
+                   sizeof (AudioChannelDescription) * audioChannelLayout->mNumberChannelDescriptions;
+#else
+            ARA_INTERNAL_ASSERT (false && "Core Audio data types can only be used on Apple platforms");
+            return 0;
+#endif
+        }
+        case kARAChannelArrangementAAXStemFormat:
+        {
+            return sizeof (uint32_t);
+        }
+        case kARAChannelArrangementCLAPChannelMap:
+        {
+            return static_cast<ARASize> (channelCount) * sizeof (uint8_t);
+        }
+        case kARAChannelArrangementCLAPAmbisonicInfo:
+        {
+            // copied from ambisonic.h to avoid dependency on CLAP SDK in this library
+            typedef struct clap_ambisonic_info {
+               uint32_t ordering;
+               uint32_t normalization;
+            } clap_ambisonic_info_t;
+            return sizeof (clap_ambisonic_info_t);
+        }
+        default:
+        {
+            ARA_INTERNAL_ASSERT (false && "unknown channel arrangement data type");
+            return 0;
+        }
+    }
+}
+
+ARAChannelCount ChannelFormat::_getImpliedChannelCount (const ARAChannelArrangementDataType channelArrangementDataType,
+                                                        const void* const channelArrangement) noexcept
+{
+    switch (channelArrangementDataType)
+    {
+        case kARAChannelArrangementUndefined:
+        {
+            return 0;
+        }
+        case kARAChannelArrangementVST3SpeakerArrangement:
+        {
+            // copied from Steinberg::Vst::SpeakerArr::getChannelCount () to avoid dependency on VST3 SDK in this library
+            auto speakerArrangement { *static_cast<const uint64_t*> (channelArrangement) };
+            ARAChannelCount arrangementChannelCount { 0 };
+            while (speakerArrangement)
+            {
+                if ((speakerArrangement & 1UL) != 0)
+                    ++arrangementChannelCount;
+                speakerArrangement >>= 1;
+            }
+            return arrangementChannelCount;
+        }
+        case kARAChannelArrangementCoreAudioChannelLayout:
+        {
+#if defined (__APPLE__)
+            const auto audioChannelLayout { static_cast<const AudioChannelLayout*> (channelArrangement) };
+            const auto layoutChannelCount { (audioChannelLayout->mChannelLayoutTag == kAudioChannelLayoutTag_UseChannelDescriptions) ?
+                                                    audioChannelLayout->mNumberChannelDescriptions :
+                                                    AudioChannelLayoutTag_GetNumberOfChannels (audioChannelLayout->mChannelLayoutTag) };
+            return static_cast<ARAChannelCount> (layoutChannelCount);
+#else
+            return 0;
+#endif
+        }
+        case kARAChannelArrangementAAXStemFormat:
+        {
+            const auto stemFormat { *static_cast<const uint32_t*> (channelArrangement) };
+            // copied from AAX_STEM_FORMAT_CHANNEL_COUNT() to avoid dependency on AAX SDK in this library
+            return static_cast<uint16_t> (stemFormat & 0xFFFF);
+        }
+        case kARAChannelArrangementCLAPChannelMap:
+        case kARAChannelArrangementCLAPAmbisonicInfo:
+        {
+            return 0;   // in CLAP, the format information does not encode the channel count
+        }
+        default:
+        {
+            ARA_INTERNAL_ASSERT (false && "unknown channel arrangement data type");
+            return 0;
+        }
+    }
+}
+
+bool ChannelFormat::isValid () const noexcept
+{
+    if (_channelArrangementDataType == kARAChannelArrangementUndefined)
+    {
+        // for undefined arrangement, the data pointer must be NULL
+        if (_channelArrangement != nullptr)
+            return false;
+
+        // for more than stereo, a valid arrangement must be provided
+        if (_channelCount > 2)
+            return false;
+    }
+    else
+    {
+        if (_channelArrangement == nullptr)
+            return false;
+
+        const auto impliedChannelCount { _getImpliedChannelCount (_channelArrangementDataType, _channelArrangement) };
+        if ((impliedChannelCount != 0) &&
+            (impliedChannelCount != _channelCount))
+            return false;
+
+        if (_channelArrangementDataType == kARAChannelArrangementCoreAudioChannelLayout)
+        {
+#if defined (__APPLE__)
+            // kAudioChannelLayoutTag_UseChannelBitmap is not allowed for Audio Units
+            const auto audioChannelLayout { static_cast<const AudioChannelLayout*> (_channelArrangement) };
+            if (audioChannelLayout->mChannelLayoutTag == kAudioChannelLayoutTag_UseChannelBitmap)
+                return false;
+            // kAudioChannelLayoutTag_UseChannelDescriptions requires a description for each channel
+            if (audioChannelLayout->mChannelLayoutTag == kAudioChannelLayoutTag_UseChannelDescriptions)
+                return audioChannelLayout->mNumberChannelDescriptions == static_cast<UInt32> (_channelCount);
+            else
+                return audioChannelLayout->mNumberChannelDescriptions == 0;
+#else
+            ARA_INTERNAL_ASSERT (false && "Core Audio data types can only be used on Apple platforms");
+            return false;
+#endif
+        }
+    }
+    return true;
+}
+
+}   // namespace ARA
---
+++ src/common/ara-ipc/ARAChannelFormat.h
@@ -0,0 +1,96 @@
+//------------------------------------------------------------------------------
+//! \file       ARAChannelFormat.h
+//!             utility class dealing with the companion-API-dependent surround channel arrangements
+//! \project    ARA SDK Library
+//! \copyright  Copyright (c) 2021-2026, Celemony Software GmbH, All Rights Reserved.
+//! \license    Licensed under the Apache License, Version 2.0 (the "License");
+//!             you may not use this file except in compliance with the License.
+//!             You may obtain a copy of the License at
+//!
+//!               http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!             Unless required by applicable law or agreed to in writing, software
+//!             distributed under the License is distributed on an "AS IS" BASIS,
+//!             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!             See the License for the specific language governing permissions and
+//!             limitations under the License.
+//------------------------------------------------------------------------------
+
+#ifndef ARAChannelArrangement_h
+#define ARAChannelArrangement_h
+
+#include "ARA_API/ARAInterface.h"
+
+namespace ARA {
+
+//! @addtogroup ARA_Library_Utility_Channel_Arrangement
+//! @{
+
+//! Helper class to parse the opaque, companion-API-dependent channel arrangement information.
+class ChannelFormat
+{
+public:
+    //! Construct with channel count (defaults to 0) and unspecified channel arrangement.
+    explicit ChannelFormat (const ARAChannelCount channelCount = 0)
+    : _channelCount { channelCount },
+      _channelArrangementDataType { kARAChannelArrangementUndefined },
+      _channelArrangement { nullptr }
+    {}
+
+    //! Construct with channel count and explicit channel arrangement.
+    ChannelFormat (const ARAChannelCount channelCount,
+                   const ARAChannelArrangementDataType channelArrangementDataType,
+                   const void* const channelArrangement)
+    : ChannelFormat { channelCount }
+    { update (channelCount, channelArrangementDataType, channelArrangement); }
+
+    ChannelFormat (const ChannelFormat& other)
+    : ChannelFormat { other.getChannelCount () }
+    { update (other.getChannelCount (), other.getChannelArrangementDataType (), other.getChannelArrangement ()); }
+
+    ~ChannelFormat () { update (_channelCount, kARAChannelArrangementUndefined, nullptr); }
+
+    //! Assign new state.
+    void update (const ARAChannelCount channelCount,
+                 const ARAChannelArrangementDataType channelArrangementDataType,
+                 const void* const channelArrangement);
+
+    //! Validation helper for use at API boundary.
+    bool isValid () const noexcept;
+
+    //! Equality tests.
+    bool operator== (const ChannelFormat& other) const noexcept;
+    bool operator!= (const ChannelFormat& other) const noexcept { return !(*this == other); }
+
+    //! Returns the channel count.
+    ARAChannelCount getChannelCount () const noexcept { return _channelCount; }
+
+    //! Returns the channel arrangement data type.
+    ARAChannelArrangementDataType getChannelArrangementDataType () const noexcept { return _channelArrangementDataType; }
+
+    //! Returns the companion-API-dependent channel arrangement data.
+    const void* getChannelArrangement () const noexcept { return _channelArrangement; }
+
+    //! Returns the size of the opaque channel arrangement data.
+    static ARASize getChannelArrangementDataSize (const ARAChannelCount channelCount,
+                                                  const ARAChannelArrangementDataType channelArrangementDataType,
+                                                  const void* const channelArrangement) noexcept;
+
+private:
+    // Returns the channel count encoded in the channel arrangement data.
+    // May return 0 if the arrangement data does not imply a certain channel count.
+    static ARAChannelCount _getImpliedChannelCount (const ARAChannelArrangementDataType channelArrangementDataType,
+                                                    const void* const channelArrangement) noexcept;
+
+private:
+    ARAChannelCount _channelCount;
+    ARAChannelArrangementDataType _channelArrangementDataType;
+    void* _channelArrangement;
+    ARAByte _arrangementStorage[24];
+};
+
+//! @} ARA_Library_Utility_Channel_Arrangement
+
+}   // namespace ARA
+
+#endif // ARAChannelArrangement_h
